bf141fd5ed06cdefd2c26bdc2ed738e27d1668a3
Jyothi Swaroop Makena <joeswaroopmakena@gmail.com>
2022-06-16 20:20:10 +0530

    Projects added using Netilfy

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const sideBar = document.querySelector('.side-bar')
 const closeBtn = document.querySelector('.close-btn');
 const articles = document.querySelectorAll('.article');
 const documentation = document.querySelectorAll('#documentation');
-
+// const articles = document.querySelectorAll('.article');
 
 
 
@@ -18,8 +18,29 @@ closeBtn.addEventListener('click', () =>{
 
 articles.forEach((article) =>{
 article.addEventListener('click', () =>{
-    console.log(article.dataset.id);
-    window.open(`${article.dataset.id}.html`);
+    if(!article.classList.contains('websites')){
+        console.log(article.dataset.id);
+        window.open(`${article.dataset.id}.html`);
+
+    }
+    else{
+        if(article.dataset.id === 'our-menu'){
+            window.open('https://astounding-pudding-b47985.netlify.app/');
+        }
+        if(article.dataset.id === 'back-roads'){
+            window.open('https://dashing-salmiakki-41c114.netlify.app/');
+        }
+        if(article.dataset.id === 'about-page'){
+            window.open('https://fastidious-sherbet-ec5b13.netlify.app/');
+        }
+        if(article.dataset.id === 'questions'){
+            window.open('https://leafy-fox-5c6823.netlify.app/');
+        }
+        if(article.dataset.id === 'video-project'){
+            window.open('https://delicate-marshmallow-7afa8f.netlify.app/');
+        }
+    }
+
 })
 })
 
